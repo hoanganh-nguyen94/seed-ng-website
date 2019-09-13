aaaa9cea63f094d352e4788b0647794051c4c98d
hoang anh <hoanganh.nguyen@the-hash.com>
2019-09-13 16:46:22 +0700

    add robot.txt and config service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {RouterModule} from '@angular/router';
 import {HuskyComponent} from './husky/husky.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -20,6 +22,7 @@ import {HuskyComponent} from './husky/husky.component';
           {path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
           {path: 'lazy/nested', loadChildren: './lazy/lazy.module#LazyModule'}
       ]),
+      ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
   ],
   providers: [],
   bootstrap: [AppComponent]
