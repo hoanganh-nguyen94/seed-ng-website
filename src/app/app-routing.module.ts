c860e267474684c84133966d59d569065782338d
Hoang Anh <hoanganh.nguyen@the-hash.com>
2021-04-10 11:59:08 +0700

    add scully

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,8 @@ import {AppComponent} from './app.component';
 import {ScullyLibModule} from '@scullyio/ng-lib';
 import {RouterModule, Routes} from "@angular/router";
 
-const appRoutes: Routes = [
-    {path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
-    {path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)}
+const appRoutes: Routes = [{ path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) }
+
 ];
 
 @NgModule({
